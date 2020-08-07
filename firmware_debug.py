54f21d4e02f3e3c12171958befb6cc8cb3aa2536
Lukas Vasadi <lukas.vasadi@gmail.com>
2020-08-07 15:45:38 +0100

    Update debug script

--- firmware_debug.py
+++ firmware_debug.py
@@ -1,7 +1,6 @@
 import csv
 import sys
 import platform
-import os
 # from PyQt5.QtGui import *
 # from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
@@ -11,6 +10,72 @@ import time
 
 import qtmodern.styles
 import qtmodern.windows
+
+
+def reader_connect():
+    # Initialize port
+    port = ""
+    # Automatically connect
+    try:
+        ports_available = list(list_ports.comports())
+        if platform.system() == "Windows":
+            for com in ports_available:
+                if "USB Serial Port" in com.description:
+                    port = com[0]
+        elif platform.system() == "Darwin" or "Linux":
+            for com in ports_available:
+                if "FT232R USB UART" in com.description:
+                    port = com[0]
+        else:
+            port = "COM30"
+
+        reader = serial.Serial(port=port, baudrate=500000, timeout=1)
+        # Toggle DTR to reset microcontroller — important for cleaning serial buffer
+        reader.setDTR(False)
+        time.sleep(0.1)
+        # Wipe serial buffer
+        reader.reset_input_buffer()
+        reader.reset_output_buffer()
+        reader.setDTR(True)
+        return reader
+
+    except AttributeError:
+        error = "AttributeError in Python, cannot detect reader"
+        print(error)
+
+def data_save(reader):
+    fieldnames = ['time', 'sen1Ch1', 'sen1Ch2', 'sen1Ch3', 'sen1Ch4', 'sen1Ch5',
+                  'sen2Ch1', 'sen2Ch2', 'sen2Ch3', 'sen2Ch4', 'sen2Ch5', 'cnt1', 'cnt2']
+    with open('data.csv', 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(fieldnames)
+
+    while True:
+        with open('data.csv', 'a', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            # time_start = time.time()
+            transmission = reader.readline()[0:-2].decode('utf-8')
+            data = transmission.split(',')
+            csv_writer.writerow([data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
+                                 data[8], data[9], data[10], data[11]])
+            # print(time.time() - time_start)
+            # time.sleep(1)
+
+            if not transmission:
+                print("Finished")
+                break
+
+def data_print(reader):
+    while True:
+        # time_start = time.time()
+        transmission = reader.readline()[0:-2].decode('utf-8')
+        print(transmission)
+        # print(time.time() - time_start)
+        # time.sleep(1)
+
+        if not transmission:
+            print("Finished")
+            break
 
 
 class Window(QMainWindow):
@@ -43,7 +108,7 @@ class Window(QMainWindow):
         self.setCentralWidget(QWidget(self))
         self.centralWidget().setLayout(self.layout)
 
-        self.btn_setup.clicked.connect(self.transmit_setup)
+        self.btn_setup.clicked.connect(self.main)
 
         self.layout.addWidget(self.lbl_reader_setting, 0, 0)
         self.layout.addWidget(self.txt_reader_setting, 0, 1)
@@ -62,48 +127,6 @@ class Window(QMainWindow):
         self.show()
 
     # IMPORTANT: When serial port opened, Arduino automatically resets
-    def transmit_setup(self):
-        if platform.system() == "Windows":
-            ports_available = list(list_ports.comports())
-            for com in ports_available:
-                if "USB Serial Port" in com.description:
-                    port = com[0]
-                    print(port)
-                    try:
-                        self.reader = serial.Serial(port=port, baudrate=500000, timeout=1)
-                        # Toggle DTR to reset ATmega328 — important for cleaning serial buffer
-                        self.reader.setDTR(False)
-                        time.sleep(0.1)
-                        # Wipe serial buffer
-                        self.reader.reset_input_buffer()
-                        # micro_controller.reset_output_buffer()
-                        self.reader.setDTR(True)
-                    except WindowsError:
-                        print("Error: Problem with serial connection")
-        else:
-            reader = serial.Serial(port="com42", baudrate=500000, timeout=1)
-            # Toggle DTR to reset ATmega328 — important for cleaning serial buffer
-            reader.setDTR(False)
-            time.sleep(0.1)
-            # Wipe serial buffer
-            reader.reset_input_buffer()
-            # micro_controller.reset_output_buffer()
-            reader.setDTR(True)
-
-        time.sleep(1)
-        transmission_incoming = True
-        while transmission_incoming:
-            transmission = self.reader.readline()[0:-2].decode("utf-8")
-            # transmission = reader.readline().decode('ascii').strip()
-            print(transmission)
-            # print(transmission.decode('utf-8'))
-            transmission_incoming = False
-
-        setup_commands = self.package_setup_commands()
-        self.reader.write(setup_commands.encode())
-
-        time.sleep(1)
-        transmit_data(self.reader)
 
     def package_setup_commands(self):
         # model = self.txt_reader_model.text()
@@ -116,30 +139,23 @@ class Window(QMainWindow):
         print("Setup: " + setup_commands)
         return setup_commands
 
+    def main(self):
+        # Connect reader
+        reader = reader_connect()
+        time.sleep(1)
 
-def transmit_data(reader):
-    fieldnames = ['time', 'sen1Ch1', 'sen1Ch2', 'sen1Ch3', 'sen1Ch4', 'sen1Ch5',
-                  'sen2Ch1', 'sen2Ch2', 'sen2Ch3', 'sen2Ch4', 'sen2Ch5', 'cnt1', 'cnt2']
-    with open('data.csv', 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(fieldnames)
+        # Confirm ready statement from reader
+        transmission = reader.readline()[0:-2].decode("utf-8")
+        # transmission = reader.readline().decode('ascii').strip()
+        print(transmission)
 
-    while True:
-        with open('data.csv', 'a', newline='') as csv_file:
-            # csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            time_start = time.time()
-            transmission = reader.readline()[0:-2].decode('utf-8')
-            print(transmission)
-            data = transmission.split(',')
-            # csv_writer.writerow([data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
-            #                      data[8], data[9], data[10], data[11]])
-            # print(time.time() - time_start)
-            # time.sleep(1)
+        # Pass setup commands
+        setup_commands = self.package_setup_commands()
+        reader.write(setup_commands.encode())
+        time.sleep(1)
 
-            if not transmission:
-                print("Finished")
-                break
+        # Print incoming data
+        data_print(reader)
 
 
 if __name__ == '__main__':
